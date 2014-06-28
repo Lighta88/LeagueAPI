94e4127573b001d80b9edd32970b1ff532f6d1a3
Lighta88 <heburiguyu@mailpick.biz>
2014-06-29 00:02:03 +0200

    Secound commit

--- LeagueAPI/src/Test.java
+++ LeagueAPI/src/Test.java
@@ -1,9 +1,14 @@
 
 public class Test {
 
+	public static void add(int a, int b){
+		System.out.println("Result: "+ (a+b));
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		add(3,5);
 	}
 
 }
